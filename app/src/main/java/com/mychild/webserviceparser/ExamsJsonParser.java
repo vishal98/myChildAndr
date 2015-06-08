740bb59a50f13147cf2394b57203bd1d3d1f05fc
Antony-droid <www.an2ny@gmail.com>
2015-06-08 19:48:17 +0530

    just commited the class file.. did no changes

--- app/src/main/java/com/mychild/webserviceparser/ExamsJsonParser.java
+++ app/src/main/java/com/mychild/webserviceparser/ExamsJsonParser.java
@@ -1,7 +1,10 @@
 package com.mychild.webserviceparser;
 
+import android.util.Log;
+
 import com.mychild.model.ExamModel;
 import com.mychild.model.ExamScheduleModel;
+import com.mychild.model.ResultsModel;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -14,6 +17,7 @@ import java.util.ArrayList;
  */
 public class ExamsJsonParser {
     public static ExamsJsonParser examsJsonParser = null;
+
 
     private ExamsJsonParser() {
 
@@ -28,41 +32,86 @@ public class ExamsJsonParser {
 
     public ArrayList<ExamModel> getExamsList(JSONObject responseJson) {
         ArrayList<ExamModel> examsList = new ArrayList<ExamModel>();
+        ArrayList<ResultsModel> childResultList = new ArrayList<ResultsModel>();
         try {
             //JSONObject responseJson = new JSONObject(str);
-            if (responseJson.has("exams")) {
-                JSONArray examsArray = responseJson.getJSONArray("exams");
+            if (responseJson.has("exam")) {
+                JSONArray examsArray = responseJson.getJSONArray("exam");
                 int examsScheduleSize = examsArray.length();
                 for (int i = 0; i < examsScheduleSize; i++) {
                     JSONObject examObj = examsArray.getJSONObject(i);
                     ExamModel examModel = new ExamModel();
+
                     if (examObj.has("examId")) {
                         examModel.setExamId(examObj.getString("examId"));
                     }
                     if (examObj.has("examType")) {
                         examModel.setExamType(examObj.getString("examType"));
                     }
-                    if (examObj.has("schedule")) {
-                        JSONArray examScheduleArray = examObj.getJSONArray("schedule");
+
+//                    if (examObj.has("result")) {
+//                        Log.d("result::", "result");
+//                        JSONArray resultArray = examObj.getJSONArray("result");
+//                        for (int k = 0; k < resultArray.length(); k++) {
+//                            JSONObject resultArrayObj = resultArray.getJSONObject(k);
+//                            ResultsModel resultsModel = new ResultsModel();
+//                            if (resultArrayObj.has("grade")) {
+//                                resultsModel.setResultGrade(resultArrayObj.getString("grade"));
+//                            }
+//
+//                            if (resultArrayObj.has("subjectName")) {
+//                                resultsModel.setResultSubjectName(resultArrayObj.getString("subjectName"));
+//                            }
+//
+//                            if (resultArrayObj.has("status")) {
+//                                resultsModel.setResultStatus(resultArrayObj.getString("status"));
+//                            }
+//
+//                            if (resultArrayObj.has("maxMarks")) {
+//                                resultsModel.setResultmaxMarks(resultArrayObj.getString("maxMarks"));
+//                            }
+//
+//                            if (resultArrayObj.has("marks")) {
+//                                resultsModel.setResultMarks(resultArrayObj.getString("marks"));
+//                                Log.i("marks>>>>>",resultsModel.getResultMarks());
+//
+//                            }
+//                            childResultList.add(resultsModel);
+//                            //resultsModel = null;
+//                        }
+//
+//                    }
+
+                    if (examObj.has("examSchedule")) {
+                        JSONArray examScheduleArray = examObj.getJSONArray("examSchedule");
                         ArrayList<ExamScheduleModel> examsScheduleList = new ArrayList<ExamScheduleModel>();
                         int subjectsSize = examScheduleArray.length();
                         for (int j = 0; j < subjectsSize; j++) {
                             JSONObject subjectObj = examScheduleArray.getJSONObject(j);
                             ExamScheduleModel examScheduleModel = new ExamScheduleModel();
-                            if (subjectObj.has("subjectName")) {
-                                examScheduleModel.setSubjectName(subjectObj.getString("subjectName"));
+
+                            if (subjectObj.has("syllabus")) {
+
+                                    JSONObject syllabusObj = subjectObj.getJSONObject("syllabus");
+                                    if (syllabusObj.has("syllabus")) {
+                                        examScheduleModel.setSubjectSyllabus(syllabusObj.getString("syllabus"));
+                                    }
                             }
-                            if (subjectObj.has("subjectSyllabus")) {
-                                examScheduleModel.setSubjectSyllabus(subjectObj.getString("subjectSyllabus"));
+
+                            if (subjectObj.has("subject")) {
+
+                                JSONObject syllabusSubjectObj = subjectObj.getJSONObject("subject");
+                                if (syllabusSubjectObj.has("subjectName")) {
+                                    examScheduleModel.setSubjectName(syllabusSubjectObj.getString("subjectName"));
+                                }
                             }
-                            if (subjectObj.has("teacherName")) {
-                                examScheduleModel.setTeacherName(subjectObj.getString("teacherName"));
+
+
+                            if (subjectObj.has("startTime")) {
+                                examScheduleModel.setExamsStartTime(subjectObj.getString("startTime"));
                             }
-                            if (subjectObj.has("examStartTime")) {
-                                examScheduleModel.setExamsStartTime(subjectObj.getString("examStartTime"));
-                            }
-                            if (subjectObj.has("examEndTime")) {
-                                examScheduleModel.setExamsEndTime(subjectObj.getString("examEndTime"));
+                            if (subjectObj.has("endTime")) {
+                                examScheduleModel.setExamsEndTime(subjectObj.getString("endTime"));
                             }
                             if (subjectObj.has("examDate")) {
                                 examScheduleModel.setExamsDate(subjectObj.getString("examDate"));
@@ -72,6 +121,7 @@ public class ExamsJsonParser {
                             examScheduleModel = null;
                         }
                         examModel.setExamScheduleList(examsScheduleList);
+                        examModel.setChildResultModel(childResultList);
                         examsScheduleList = null;
                     }
                     examsList.add(examModel);
@@ -81,5 +131,58 @@ public class ExamsJsonParser {
             e.printStackTrace();
         }
         return examsList;
+    }
+
+
+
+    public ArrayList<ResultsModel> getResultList(JSONObject responseJson){
+        ArrayList<ResultsModel> childResultList = new ArrayList<ResultsModel>();
+
+        try {
+
+            if (responseJson.has("exam")) {
+                JSONArray examsArray = responseJson.getJSONArray("exam");
+                int examsScheduleSize = examsArray.length();
+                for (int i = 0; i < examsScheduleSize; i++) {
+                    JSONObject examObj = examsArray.getJSONObject(i);
+
+                    if (examObj.has("result")) {
+                        Log.d("result::", "result");
+                        JSONArray resultArray = examObj.getJSONArray("result");
+                        for (int k = 0; k < resultArray.length(); k++) {
+                            JSONObject resultArrayObj = resultArray.getJSONObject(k);
+                            ResultsModel resultsModel = new ResultsModel();
+                            if (resultArrayObj.has("grade")) {
+                                resultsModel.setResultGrade(resultArrayObj.getString("grade"));
+                            }
+
+                            if (resultArrayObj.has("subjectName")) {
+                                resultsModel.setResultSubjectName(resultArrayObj.getString("subjectName"));
+                            }
+
+                            if (resultArrayObj.has("status")) {
+                                resultsModel.setResultStatus(resultArrayObj.getString("status"));
+                            }
+
+                            if (resultArrayObj.has("maxMarks")) {
+                                resultsModel.setResultmaxMarks(resultArrayObj.getString("maxMarks"));
+                            }
+
+                            if (resultArrayObj.has("marks")) {
+                                resultsModel.setResultMarks(resultArrayObj.getString("marks"));
+                                Log.i("marks>>>>>",resultsModel.getResultMarks());
+
+                            }
+                            childResultList.add(resultsModel);
+                            //resultsModel = null;
+                        }
+
+                    }
+                }
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return childResultList;
     }
 }
