bedceb3955bbbce860f2ff2a60f7928df5f7e7fb
vish98 <vishal1398@yahoo.co.in>
2015-07-31 13:19:13 +0530

    latest changes

--- app/src/main/java/com/mychild/interfaces/IOnExamChangedListener.java
+++
@@ -1,8 +0,0 @@
-package com.mychild.interfaces;
-
-/**
- * Created by Sandeep on 30-03-2015.
- */
-public interface IOnExamChangedListener {
-    public void onExamChanged(int position, boolean isChecked);
-}
